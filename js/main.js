b12176f9f52b5a09a611ec2c6c5e553b8020f06b
Herbeson Queiroz <qherbeson@gmail.com>
2023-07-17 11:20:25 -0300

    documentos modificados

--- js/main.js
+++ js/main.js
@@ -40,4 +40,27 @@ const timer = setInterval(updateNumber, interval);
 animaNumero(animatedNumber1, 598);
 animaNumero(animatedNumber2, 73);
 animaNumero(animatedNumber3, 4);
-animaNumero(animatedNumber4, 7)
+animaNumero(animatedNumber4, 7);
+
+// ========================CARROSSEL DE DEPOIMENTOS=============
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
